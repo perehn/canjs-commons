c6d7693d6fac3b9f611cc54b8cf97a196e6f82e0
perehn <per.ehnbom@gmail.com>
2014-09-26 16:54:11 +0200

    extensions

---
+++ canjs-commons/extensions.js
@@ -0,0 +1,50 @@
+define(['jquery',
+        'can'],
+        function(jQuery, can){
+
+	can.Control.prototype.find = function(s){
+		return this.element.find(s);  
+	};
+
+	can.Model.List.prototype.getOne = function(id){
+		return this.filter(function(element){
+			return element.id == id;
+		})[0];
+	};
+	can.Model.List.prototype.sort = function(sortFunc){
+
+		[].sort.apply(this, [sortFunc]); 
+		return this;
+	};
+	can.Model.List.prototype.remove = function(item){
+
+		var index = this.indexOf(item);
+		this.splice(index, 1);
+		console.log(index);
+	};
+
+
+
+	jQuery.fn.model = function(m){
+		if(m){
+
+			this.data('model', m);
+			this.addClass(m.constructor._shortName);
+			this.addClass(m.constructor._shortName + "_" + m.id);
+			return this;
+		}else{
+			return this.data('model');
+		}
+	};
+
+
+	jQuery.Event.prototype.stop = function(){
+		this.stopPropagation();
+		this.preventDefault();
+	};
+
+
+
+
+
+})
